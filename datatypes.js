344635122ef69d078c6089b49b31c3cd145935c2
Karlis.Kade <karlis.kade@voco.ee>
2023-09-19 13:52:46 +0300

    datatypes and conv

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+// data types
+// String
+let eesNimi = "Karlis"
+let pereNimi = " Kade"
+console.log(eesNimi + pereNimi)
+console.log(typeof eesNimi)
+
+// Numbers
+let goodMark = 5
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+// Boolean
+let isToday = true
+console.log(typeof isToday)
+
+// Type conv
+// num  to string
+console.log(typeof String(555))
+console.log(String(5+5))
+console.log(String(5.5) + String(5.5))
+
+// boolean to string
+console.log(typeof String(true))
+
+// string to num
+console.log(Number('666'))
+console.log(Number('66.6'))
+console.log(parseInt(100.3))
+console.log(parseFloat(100.3))
