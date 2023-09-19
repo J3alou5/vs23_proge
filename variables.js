818e9ee5926b801cf447a9304d52874c52044a3f
Karlis.Kade <karlis.kade@voco.ee>
2023-09-19 12:01:57 +0300

    js variables

---
+++ variables.js
@@ -0,0 +1,23 @@
+// varibels
+// keywors varibalename = value
+// let
+
+
+// declare
+let programmingLanguage = 'Java Script'
+console.log(programmingLanguage)
+
+
+// assignment - save new valu into the variable
+programmingLanguage = 'PHP'
+console.log(programmingLanguage)
+let vastus = programmingLanguage + programmingLanguage
+console.log(vastus)
+
+let result
+console.log(result)
+
+// const
+const group = 'VS23'
+group = 'PG23'
+console.log(group)
