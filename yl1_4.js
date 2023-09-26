281dcee1470f2ebd0a5e11942a74d5d0b0e46cdd
Karlis.Kade <karlis.kade@voco.ee>
2023-09-26 14:41:54 +0300

    ul 4

--- yl1_4.js
+++ yl1_4.js
@@ -8,7 +8,7 @@ const readline = require('readline').createInterface({
 readline.question('Sisestage oma nimi: ', name => {
 	readline.question('Sisestage lubatud kiirus (km/h): ', allowedSpeed  => {
 		readline.question('Sisestage reaalne kiirus (km/h): ', realSpeed  => {
-			let esialgneTrahv = (realSpeed - allowedSpeed) * 3
+			let esialgneTrahv = (parseInt(realSpeed) - parseInt(allowedSpeed)) * 3
 			let lause = name + ", kiiruse uletamise eest on Teie trahv " + Math.min(190,esialgneTrahv) + " eurot." 
 		console.log(lause);
 		readline.close();
