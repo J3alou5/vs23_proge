7720bbd81083b03f000be56871b47a77fc00cad8
Karlis.Kade <karlis.kade@voco.ee>
2023-09-26 13:19:06 +0300

    ul 2

---
+++ yl1_2.js
@@ -0,0 +1,7 @@
+// ulesanne 1.2
+
+let aasta = "2016"
+let president = "Kersti Kaljulaid"
+let lauseKeskosa = ". aastal valiti Eesti presidendiks "
+lause = aasta + lauseKeskosa + president
+console.log(lause)
